a2754eb9411728b001fb5001ebe37bd92ca1d1ee
nahla <nahlanabil84@gmail.com>
2023-05-14 02:43:24 +0300

    base creation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pet_shop/styles.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -22,16 +25,73 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[],
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: colorBackground,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const SizedBox(
+              height: 10.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SvgPicture.asset("assets/nav_icon.svg", height: 18.0),
+                  const CircleAvatar(
+                    radius: 20.0,
+                    backgroundColor: colorAvatarBackground,
+                    backgroundImage: NetworkImage(
+                        "https://img.freepik.com/premium-vector/pretty-hijab-woman-side-profile-with-colorful-flower-bouquet_185694-1105.jpg?w=2000"),
+                  )
+                ],
+              ),
+            ),
+          ],
         ),
+        bottomNavigationBar: buildBottomNavigationBar(),
       ),
+    );
+  }
+
+  BottomNavigationBar buildBottomNavigationBar() {
+    return BottomNavigationBar(
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: _selectedIndex == 0
+              ? SvgPicture.asset('assets/home_selected.svg')
+              : SvgPicture.asset('assets/home_unselected.svg'),
+          label: '',
+        ),
+        BottomNavigationBarItem(
+            icon: _selectedIndex == 1
+                ? SvgPicture.asset('assets/cart_selected.svg')
+                : SvgPicture.asset('assets/cart_unselected.svg'),
+            label: ''),
+        BottomNavigationBarItem(
+            icon: _selectedIndex == 2
+                ? SvgPicture.asset('assets/profile_selected.svg')
+                : SvgPicture.asset('assets/profile_unselected.svg'),
+            label: ''),
+      ],
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
+      backgroundColor: colorWhite,
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
     );
   }
 }
---
+++ lib/styles.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+//Colors
+const Color colorBackground = Color(0xfffff4f4);
+
+const Color colorAvatarBackground = Color(0xffFC3366);
+
+const Color colorDarkGrey = Color(0xff646379);
+const Color colorLightGrey = Color(0xffA4A4AF);
+
+const Color colorBlack = Color(0xff0a0a0a);
+
+const Color colorWhite = Color(0xfffef9f9);
+
+const Color colorOrange = Color(0xffECA27E);
+const Color colorDarkOrange = Color(0xffEA4B4B);
+const Color colorLightOrange = Color(0xffFFD0B9);
+
+final fontSourceSansProLight = GoogleFonts.sourceSansPro(
+  fontWeight: FontWeight.w300,
+);
+
+final fontSourceSansPro = GoogleFonts.sourceSansPro(
+  fontWeight: FontWeight.w400,
+);
+
+final fontSourceSansProMedium = GoogleFonts.sourceSansPro(
+  fontWeight: FontWeight.w500,
+);
+
+final fontSourceSansProBold = GoogleFonts.sourceSansPro(
+  fontWeight: FontWeight.w600,
+);
+
+final fontSourceSansProBlack = GoogleFonts.sourceSansPro(
+  fontWeight: FontWeight.w700,
+);
